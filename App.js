6ac193a05bf48d47456de84a185b95d3662810e0
Cristian Araujo <araujocristian@live.com>
2019-09-23 17:13:54 -0300

    add axios and init list

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import {View, Text, FlatList} from 'react-native';
 import axios from 'axios';
 
 import Header from './src/components/Header';
 
 const App = () => {
-  const names = axios.get('https://randomuser.me/api/?nat=br&results=5');
+  const [peoples, setPeoples] = useState([]);
+
+  const loadUsers = async () => {
+    const response = await axios.get(
+      'https://randomuser.me/api/?nat=br&results=5',
+    );
+
+    const {results} = response.data;
+
+    setPeoples(results);
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
   return (
     <Fragment>
       <Header title="Pessoas" />
       <FlatList
-        data={names}
-        renderItem={({item}) => <Text>{item}</Text>}
-        keyExtractor={item => item}
+        data={peoples}
+        renderItem={({item}) => <Text>{item.name.first}</Text>}
+        keyExtractor={item => item.email}
       />
     </Fragment>
   );
