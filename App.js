10eb273b26b83b8a5bead865a1c37fa255224393
Cristian Araujo <araujocristian@live.com>
2019-09-23 16:58:22 -0300

    add Flat List and init axios

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
+import axios from 'axios';
 
 import Header from './src/components/Header';
 
 const App = () => {
+  const names = axios.get('https://randomuser.me/api/?nat=br&results=5');
   return (
     <Fragment>
       <Header title="Pessoas" />
+      <FlatList
+        data={names}
+        renderItem={({item}) => <Text>{item}</Text>}
+        keyExtractor={item => item}
+      />
     </Fragment>
   );
 };
